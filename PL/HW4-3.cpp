56ffcc30f81fd282b0d1fc2ca912e79fb782aa98
chang yu <100700089+changfish@users.noreply.github.com>
2023-04-06 17:29:41 +0800

    20230406 終於看懂題目惹

--- PL/HW4-3.cpp
+++ PL/HW4-3.cpp
@@ -3,32 +3,22 @@
 #include <algorithm>
 using namespace std;
 
-bool upper(int a,int b){	//sort by upper
-	return a<b;
-}
-bool lower(int a,int b){ 	//sort by lower
-	return a>b;
-}
 
 int main(){
-	cin.tie(0);
-	string cmd;	//我很抱歉 我不知道step是什麼意思
-	int tem,n=4;//tem用來輸入，然後丟進去vector
-	vector<int>	 v;
-	cin>>cmd;
-	while(n--&&cin>>tem){	//理論上可以輸入至ctrl+z(結束)，但這邊用4個資料來演示 
-		v.push_back(tem);
-	}
-	if(cmd == "lower")					//我還是很抱歉 我看不懂step或是我就在瞎搞 
-		sort(v.begin(),v.end(),lower);	//STL sort
-	else if(cmd == "upper")
-		sort(v.begin(),v.end(),upper);	//using iterator to operate
-	
-	printf("Celsius\t\t|\tFahrenheit\n");
- 	for(int i=0;i<4;i++){
+	int lower,upper,step;
+	cout<<"A program print the conversion of Celsius-Fahrenheit\n\n";
+	cout<<"lower value:\t";
+	cin>>lower;
+	cout<<"upper value:\t";
+	cin>>upper;
+	cout<<"step value:\t";
+	cin>>step;
+	cout<<"Celsius\t\t|\tFahrenheit\n";
+	for(int i=0;i<4;i++){
  		for(int j=0;j<9;j++)
 			cout<<"-";
 	}
-	for(vector<int>::iterator it = v.begin();it!=v.end();it++)
-		printf("\n%d\t\t|\t%.2f",*it,(*it)*1.8+32.0);
+	for(int i=lower;i<=upper;i+=step){
+		printf("\n%d\t\t|\t%.2f",i,i*1.8+32.0);
+	}
 }
