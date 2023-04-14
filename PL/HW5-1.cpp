77909c565bda539f70a0b36ed1cdb72738cb9456
chang yu <100700089+changfish@users.noreply.github.com>
2023-04-14 12:04:03 +0800

    Update HW5-1.cpp

--- PL/HW5-1.cpp
+++ PL/HW5-1.cpp
@@ -3,13 +3,29 @@ using namespace std;
 
 const float pi = 3.14159;
 
+class circle{
+	public:
+		int radius;
+		float Diameter(){
+			return 2*radius;
+		}
+		float Circumference(){
+			return 2*radius*pi;
+		}
+		float Area(){
+			return radius*radius*pi;
+		}
+};
+
 int main(){
 	int r;
+	circle A;
 	cout<<"Input radius:";
 	while(cin>>r){
-		cout<<"Diameter = "<<2*r<<" units\n";
-		cout<<"Diameter = "<<2*r*pi<<" units\n";
-		cout<<"Area = "<<r*r*pi<<" sq. units\n";
+		A.radius = r;
+		cout<<"Diameter = "<<A.Diameter()<<" units\n";
+		cout<<"Circumference = "<<A.Circumference()<<" units\n";
+		cout<<"Area = "<<A.Area()<<" sq. units\n";
 		cout<<"Input radius:";
 	}
 }
