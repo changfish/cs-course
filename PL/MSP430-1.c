64c983bf99e093727c5d875a86f0d805a57ca38a
chang yu <100700089+changfish@users.noreply.github.com>
2023-05-26 12:46:44 +0800

    Create MSP430-1.c
    最初的版本

---
+++ PL/MSP430-1.c
@@ -0,0 +1,21 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    P1OUT = 0;
+    P1DIR  = BIT0+BIT6;
+
+    while(1){
+        P1OUT ^= BIT0;
+        __delay_cycles(100000);
+        P1OUT ^= BIT6;
+        __delay_cycles(100000);
+    }
+
+    return 0;
+}
